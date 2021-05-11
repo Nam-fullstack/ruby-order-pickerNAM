d3dc6abc74786ecd75c17b92ea52c4072fc30ed5
Nam-fullstack <namnguyen13_@hotmail.com>
2021-05-11 12:40:50 +1000

    set up path and file system

--- group.rb
+++ group.rb
@@ -1,8 +1,12 @@
 class Group
     attr_reader :name, :names_array
-    def initialize(name)      # overwriting the default initialize method
-        @name = name
-        @names_array = ["Alice", "Bob", "Charlie", "Dylan", "Eddie"]
+
+
+
+    def initialize(group_name, file_path)      # overwriting the default initialize method
+        @name = group_name
+        @path = file_path
+        @names_array = self.path_to_array       # using self to refer to the instance of the group
     end
 
     def randomise_order
@@ -18,8 +22,26 @@ class Group
         @names_array.push(name)
     end
 
+    def path_to_array
+        begin
+            array = File.readlines(get_file_path).map { |name| name.strip }    # reads in text from file and takes out whitespace
+        rescue 
+            puts "Invalid path, creating file"
+
+            # w for write mode
+            File.open(get_file_path, "w") do |file|
+                file.write("")
+            end
+            array = []
+        end
+        return array
+    end
+
+    private
+    def get_file_path
+        @path
+    end
 end
 
-example = Group.new("Example")
-
-pp example.randomise_order
+test_group = Group.new("Test Group", './groups/test-group.txt')
+test_group.output_random_array
